1783f83ef0ee171f8f0d2ab75dc063f94040d3af
CoreOS Admin <core@iconviet.eesi1evmtb5uhgkxwlujvbvrbc.ix.internal.cloudapp.net>
2019-07-22 13:55:01 +0000

    changed

---
+++ install_docker_compose.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mkdir -p /opt/bin
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
