32328a125c5d56f83872775ef802261e8ebabff1
CoreOS Admin <core@iconviet.eesi1evmtb5uhgkxwlujvbvrbc.ix.internal.cloudapp.net>
2019-07-24 15:17:33 +0700

    added Dockerfile and yml

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /opt/bin
-curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
-chmod +x /opt/bin/docker-compose
+sudo mkdir -p /opt/bin
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
