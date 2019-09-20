983bfb54e3e9d86f587a843b33ed323f24cd8c19
iconmunist <tomnuen@agiper.com>
2019-09-21 02:34:57 +0700

    Update coreos_install_docker_compose.sh

--- coreos_install_docker_compose.sh
+++ coreos_install_docker_compose.sh
@@ -2,3 +2,6 @@
 sudo mkdir -p /opt/bin
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
 sudo chmod +x /opt/bin/docker-compose
+
+#rancher
+sudo wget -O /usr/bin/docker-compose https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` && sudo chmod +x /usr/bin/docker-compose
