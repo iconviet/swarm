bc4ef973a4d56ef2f2dc3a5995aadd058484e69e
Tom Nuen <tomnuen@iconviet.io>
2019-11-10 19:09:36 +0700

    changed

--- coreos_install_docker_compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /opt/bin
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
-sudo chmod +x /opt/bin/docker-compose
-
-#rancher
-echo 'docker run --rm radial/busyboxplus:curl curl $@' > /usr/bin/curl && chmod +x /usr/bin/curl
-sudo wget -O /usr/bin/docker-compose https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` && sudo chmod +x /usr/bin/docker-compose
